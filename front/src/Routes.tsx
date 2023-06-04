da2a6f97794dda0d716e0269d6baa603794fdf6f
Matheus Santos <matheussantosspbr@gmail.com>
2023-06-03 22:06:30 -0300

    Criação da tela de Registrar e Configuração das Rotas

--- front/src/Routes.tsx
+++ front/src/Routes.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes as Router, Route } from "react-router-dom";
+import Login from "./views/Login";
+import Register from "./views/Register";
+
 export default function Routes() {
   return (
-    <></>
+    <BrowserRouter>
+      <Router>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Router>
+    </BrowserRouter>
   );
 }
--- front/src/views/Register.tsx
+++ front/src/views/Register.tsx
@@ -1,25 +1,29 @@
+import { useState } from "react";
 import Input from "../components/Input";
 import {NavLink} from 'react-router-dom'
-import { useState } from "react";
 
-export default function Login() {
-  
+export default function Register() {
+
   const [email, setEmail] = useState('')
+  const [confEmail, setConfEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  const [confPassword, setConfPassword] = useState('')
+  
   return (
     <div className="bg-black flex items-center justify-center flex-col h-[100vh]">
       <h1 className="text-white text-3xl font-semibold mb-14">
         Gerenciador de Tarefas
       </h1>
       <div>
-        <h2 className="text-white text-xl font-semibold mb-6">Login</h2>
+        <h2 className="text-white text-xl font-semibold mb-6">Registrar</h2>
         <div className="flex flex-col items-center justify-center">
           <Input type="email" placeholder="Email"/>
+          <Input type="email" placeholder="Confime seu email"/>
           <Input type="password" placeholder="Senha"/>
-          <Input type="submit" value="Entrar" others="hover:bg-cyan-500"/>
+          <Input type="password" placeholder="Confirme sua Senha"/>
+          <Input type="submit" value="Registrar" others="hover:bg-cyan-500"/>
         </div>
-        <NavLink to='/register' className="text-md text-white font-medium">Crie uma conta.</NavLink>
+        <NavLink to='/' className="text-md text-white font-medium">Fazer Login.</NavLink>
       </div>
     </div>
   );
